3a0f518908fff30df96b9f747c7df960661d9115
Javier <fjrobles@gmail.com>
2025-11-11 22:51:58 +0100

    Update T1-T2.js

--- T1-T2/T1-T2.js
+++ T1-T2/T1-T2.js
@@ -46,53 +46,6 @@ Se nos pide:
   almacenándolo en una nueva constante denominada serializedBook .
 
 Asegúrate de que cada hechizo esté documentado y que los resultados sean tan mágicos como se espera en este mundo literario encantado. ¡Que la magia te guíe!
-  	
-  -- CATALÀ --
-  -- EXERCICI DE REPÀS TEMA 1 I TEMA 2 ENUNCIAT:
-Imagineu que esteu construint un sistema únic per gestionar la biblioteca literària d'una escola molt especial.
-A partir d'un conjunt de llibres representats com a objectes amb propietats com ara title (títol), author (autor), genre (gènere), yearPublished (any de Publicacion) i available (que indica si està disponible).
-Les dades d'aquests llibres ens les faciliten en un Array a la constant library.
-Se'ns demana:
-
- * 1.- Món dels Llibres Encantats:
-  Dissenya una classe que anomenarem "magicBook" amb un constructor màgic que, en ser invocat, a part de totes les propietats anteriorment esmentades dels llibres (title, author, genre, yearPublished i available)
-  afegeixi automàticament un segell místic a cada nou llibre en una nova propietat anomenada "magicSeal"; el valor d'aquesta propietat s'ha d'obtenir cridant a un mètode de classe estàtic definit a la mateixa classe que anomenarem
-  "generateMagicSeal" el qual rebrà dos paràmetres: el títol i l'autor, a partir d'aquests paràmetres, el mètode pren les dues primeres lletres del títol i, a continuació, les dues últimes de l'autor, les combina, les converteix
-  a majúscules i afegeix "MAGIC" al final de la cadena, amb aquest valor hem de definir la propietat magicSeal al constructor.
-  
-  D'altra banda, defineix un mètode d'instància anomenat toggleAvailability que modifiqui màgicament la propietat "available" de l'objecte de manera que si està definida com true la canvii a false i viceversa en invocar aquest mètode.
-  
-  NOTA: Utilitza la "paraula màgica" this als teus encanteris dins de la classe magicBook.
-
- * 2.- map() L'Encantador: Crea un encanteri usant el mètode map() per transformar els llibres continguts en la constant "library" a llibres llegibles només per mags, de manera que en una constant que anomenarem "mysticalBooks"
-  emmagatzemem només un Array amb els valors de les propietats magicSeal de nous llibres màgics generats a partir de la informació continguda a la constant "library".
-  NOTA: Fletxes Encantades: Utilitza fletxes encantades (Arrow functions) als teus conjurs per realitzar accions més ràpides i precises dins del teu mètode map().
-
- * 3.- filter() el Cercador de Somnis : Utilitza una vareta especial (mètode filter()) per obtenir només els llibres de la constant "library" de la categoria "Fantasy" i que hagin estat publicats després de 1950. Els resultats d'aquest filtre màgic els
-  emmagatzemarem a la constant "fantasyBooksAfter1950".
-  NOTA: Fletxes Encantades: Utilitza fletxes encantades (Arrow functions) als teus conjurs per realitzar accions més ràpides i precises dins del teu mètode filter().
-
- * 4.- Invocant a l'oracle Recursiu: Invoca a l'oracle amb una funció recursiva, a la qual anomenarem "recursiveOracle" que predigui la quantitat total de llibres disponibles a la biblioteca. Aquesta funció s'ha de cridar a si mateixa passant-se com a paràmetres
-  l'Array de llibres que està emmagatzemat a la constant "library" i un valor amb l'índex de l'últim llibre que ha comprovat la funció si està disponible o no. L'objectiu és que la funció comprovi un llibre, i, si està disponible, incrementeu el comptador
-  de llibres disponibles i es torni a trucar a si mateixa rebent l'Array original de la biblioteca i la posició següent del llibre a comprovar, per, finalment, i un cop recorregut tot l'Array, tornar el total de llibres disponibles.
-
- * 5.- El Místic Refugi de Funcions: Dissenya un refugi místic on una funció exterior anomenada compareBooks() que rebi un Array de llibres (per exemple la constant "library") invoqui diversos esperits al seu interior:
-  La primera invocació serà una altra funció definida al seu interior anomenada findMostRecentBook() que retorna el llibre la publicació del qual és la més recent en el temps de tots els rebuts.
-  La segona invocació serà una altra funció definida al seu interior anomenada findLongestTitleBook() que retorna el llibre el títol del qual és el més llarg de tots els rebuts.
-  La funció compareBooks, ens tornarà, després d'invocar els seus esperits interiors, un Array amb dos elements: al primer el llibre la publicació del qual és la més recent en el temps de tots els rebuts,
-  i en el segon el llibre el títol del qual és el més llarg de tots els rebuts.
-
- * 6.- Encantament Temporal de Llibres: Dissenya un encanteri màgic (una funció anomenada earliestPublication) que accepti un nombre indeterminat de llibres (mitjançant l'ús de paràmetres rest) i retorni el llibre que va ser publicat abans en el temps.
-  Aquest encanteri haurà de ser capaç de comparar les dates de publicació dels llibres i retornar el llibre amb el llançament més antic.
-
- * 7.- Encanteri de Duplicació spread: Utilitza un encanteri de duplicació (mitjançant l'Operador spread...) per clonar l'inventari de llibres (definit a la constant library) en una nova constant anomenada clonedLibrary i conjura un nou llibre
-  i afegeix-lo a la nova biblioteca que tens a la nova constant.
-
- * 8.- Rituals de Serialització: Desenvolupa un ritual de serialització (function invokeSerialization) que rebi un llibre de la llibreria (definit a la constant "library") i el converteixi en un pergamí llegible per qualsevol ésser en el regne digital
-  emmagatzemant-lo en una nova constant anomenada serializedBook.
-
-Assegureu-vos que cada encanteri estigui documentat i que els resultats siguin tan màgics com s'espera en aquest món literari encantat. Que la màgia et guiï!
-
 */
 const library = [
   { title: "One Hundred Years of Solitude", author: "Gabriel García Márquez", genre: "Fiction", yearPublished: 1967, available: true },
